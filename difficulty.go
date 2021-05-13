a66f1ad545748a1b1f364e5f54ecc546f7acb134
gucio321 <gucio321@protonmail.com>
2021-05-13 08:08:36 +0200

    difficulty.go: export some struct fields

--- difficulty.go
+++ difficulty.go
@@ -14,34 +14,34 @@ type Difficulty map[d2enum.DifficultyType]*DifficultyLevelStatus
 
 // DifficultyLevelStatus represents a status of difficulty level
 type DifficultyLevelStatus struct {
-	act byte
+	Act byte
 	unknown3,
 	unknown4,
 	unknown5,
 	unknown6 bool
-	active bool
+	Active bool
 }
 
 // Unmarshal loads byte into DifficultyLevelStatus structure
 func (d *DifficultyLevelStatus) Unmarshal(data byte) {
 	bm := d2datautils.CreateBitMuncher([]byte{data}, 0)
-	d.act = byte(bm.GetBits(actBitsCount))
+	d.Act = byte(bm.GetBits(actBitsCount))
 	d.unknown3 = bm.GetBit() == 1
 	d.unknown4 = bm.GetBit() == 1
 	d.unknown5 = bm.GetBit() == 1
 	d.unknown6 = bm.GetBit() == 1
-	d.active = bm.GetBit() == 1
+	d.Active = bm.GetBit() == 1
 }
 
 // Encode encodes a difficulty level status back into byte data
 func (d *DifficultyLevelStatus) Encode() (result byte) {
 	sw := d2datautils.CreateStreamWriter()
-	sw.PushBits(d.act, 3)
+	sw.PushBits(d.Act, 3)
 	sw.PushBit(d.unknown3)
 	sw.PushBit(d.unknown4)
 	sw.PushBit(d.unknown5)
 	sw.PushBit(d.unknown6)
-	sw.PushBit(d.active)
+	sw.PushBit(d.Active)
 
 	return sw.GetBytes()[0]
 }
