72f3c0057e707be7c9a36c978512da8d60d7e3cd
gucio321 <gucio321@protonmail.com>
2021-05-18 13:17:48 +0200

    all: fix tests

--- d2s_test.go
+++ d2s_test.go
@@ -3,9 +3,10 @@ package d2d2s
 import (
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-/*
 func Test_Load(t *testing.T) {
 	testFile, fileErr := os.Open("testdata/example.d2s")
 
@@ -34,7 +35,6 @@ func Test_Load(t *testing.T) {
 
 	assert.Equal(t, "example", x.Name, "Unexpected name read")
 }
-*/
 
 // nolint:wsl // just test
 func Test_LoadEncode(t *testing.T) {
@@ -67,38 +67,11 @@ func Test_LoadEncode(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	// nolint:gocritic // will use later
-	/*
-		// nolint:gosec // only tests
-		err = ioutil.WriteFile("/tmp/out.d2s", d, 0o600)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		_, err = Unmarshal(d)
-		if err != nil {
-			t.Fatal(err)
-		}
-	*/
-
-	// nolint:gocritic // will use later
-	/*
-		m := 0
-		max := 16
-		fmt.Println(d[m:max])
-		fmt.Println(data[m:max])
-		// fmt.Println(len(d[m:max]), len(data[m:max]))
-		fmt.Println(bytes.Equal(data[m:max], d[m:max]))
-		t.Fail()
-	*/
 }
 
-/*
 func Test_New(t *testing.T) {
 	x := New()
 	if _, err := x.Encode(); err != nil {
 		t.Fatal(err)
 	}
 }
-*/
