7e1cddaf455247244d7cccf2097c4156e7201fd5
gucio321 <73652197+gucio321@users.noreply.github.com>
2022-03-07 21:15:50 +0100

    add MedianXL save file for testing (break tests)

--- pkg/d2s/d2s_test.go
+++ pkg/d2s/d2s_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/gucio321/d2d2s/internal/testdata"
+	"github.com/gucio321/d2d2s/pkg/d2s/d2sstats"
 )
 
 func Test_Load(t *testing.T) {
@@ -36,12 +37,39 @@ func Test_Load(t *testing.T) {
 }
 
 func Test_LoadEncode(t *testing.T) {
+	mxl := New()
+	mxl.Stats.UserStatMap(
+		map[d2sstats.StatID]int{
+			d2sstats.Strength:       11,
+			d2sstats.Energy:         11,
+			d2sstats.Dexterity:      11,
+			d2sstats.Vitality:       11,
+			d2sstats.UnusedStats:    11,
+			d2sstats.UnusedSkills:   8,
+			d2sstats.CurrentHP:      21,
+			d2sstats.MaxHP:          21,
+			d2sstats.CurrentMana:    21,
+			d2sstats.MaxMana:        21,
+			d2sstats.CurrentStamina: 21,
+			d2sstats.MaxStamina:     21,
+			d2sstats.Level:          8,
+			d2sstats.Experience:     32,
+			d2sstats.Gold:           22,
+			d2sstats.StashedGold:    25,
+			88:                      48,
+		},
+	)
+	mxl.Skills.SetSkillsCount(95)
+	mxl.Items.IgnoreErrors()
+
 	tests := []struct {
 		name     string
 		filename string
+		base     *D2S
 	}{
 		// {"empty character created by d2d2s.New()", "example.d2s"},
-		{"New character created by Diablo II: Lord of Destruction v1.14d", "newGameChar.d2s"},
+		{"New character created by Diablo II: Lord of Destruction v1.14d", "newGameChar.d2s", New()},
+		{"MedianXL char", "maxsocket.d2s", mxl},
 	}
 
 	for _, test := range tests {
@@ -51,12 +79,12 @@ func Test_LoadEncode(t *testing.T) {
 				tt.Fatal(err)
 			}
 
-			x, err := Load(data)
+			test.base.Load(data)
 			if err != nil {
 				tt.Fatal(err)
 			}
 
-			out, err := x.Encode()
+			out, err := test.base.Encode()
 			if err != nil {
 				tt.Fatal(err)
 			}
